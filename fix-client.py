1fe3f37ca6571533c7b5fffee2145415552472c3
dr4gonw4ll <anand.namana@gmail.com>
2020-06-08 16:07:03 -0400

    Update fix-client.py

--- fix-client.py
+++ fix-client.py
@@ -8,7 +8,6 @@ from datetime import datetime
 from datetime import timedelta
 import atexit
 import shelve
-import socketio
 import re
 from colorama import Fore, Back,Style
 
@@ -17,6 +16,7 @@ class client():
     def __init__(self):
         if(os.path.isfile(".cache.dat")):
             self.data=shelve.open(".cache")
+            self.updateReqSequence(sys.argv[1])
             print(self.data["reqseq"])
             print(self.data["resseq"])
             
@@ -49,8 +49,8 @@ class socketConnect():
     def __init__(self,seqObj): #Initialize the Fix message objects here
         self.seqObj = seqObj
         self.message = simplefix.FixMessage()
-        
         self.resp_mesg = simplefix.FixParser()
+
         return
     
     def header(self):  # this is header message
@@ -70,7 +70,7 @@ class socketConnect():
         logon.append_pair(35,"A")
         logon.append_pair(108,0)
         logon.append_pair(98,0)
-        self.seqObj.updateReqSequence()
+
         _temp_logon = logon
         del logon
         return _temp_logon
@@ -82,39 +82,36 @@ class socketConnect():
            
     def recvMesg(self): #function which receives the messages and processes it accordingly
         while True:
-            try:
-                self.received_message = self.s.recv(4096)
-                self.resp_mesg.append_buffer(self.received_message)
-                self.resp_mesg = self.resp_mesg.get_message()
-                print(Fore.GREEN+"Received:\r\n"+str(self.resp_mesg))
-                if self.resp_mesg.get(35,1).decode('utf-8') == "0":
-                    print("Hearbeat Response")
-                elif self.resp_mesg.get(35,1).decode('utf-8') == "1":
-                    print("Test Request")
-                    self.testreqid = self.resp_mesg.get(112,1)
-                    self.testRequest(self.testreqid)
-                elif self.resp_mesg.get(35,1).decode('utf-8') == "2":
-                    print("send resend request")
-                elif self.resp_mesg.get(35,1).decode('utf-8') == "3":
-                    print("Message Rejected, Exiting the program")
-                    exit(1)
-                elif self.resp_mesg.get(35,1).decode('utf-8') == "4":
-                    print("Sequence Reset..")
-                elif self.resp_mesg.get(35,1).decode('utf-8') == "5":
-                    print("Logout")
-                elif self.resp_mesg.get(35,1).decode('utf-8') == "A":
-                    print("Logged in successfully")
-                elif self.resp_mesg.get(35,1).decode('utf-8') == "D":
-                    print("Order Single ")
-                else:
-                    print(self.resp_mesg)
-                    print("Proceed based on response value of 35")
-                    del self.resp_mesg
-                    self.resp_mesg = simplefix.FixParser()                        
-            except:
-                print("")
-                
-          
+
+            self.received_message = self.s.recv(4096)
+            self.resp_mesg.append_buffer(self.received_message)
+            self.resp_mesg = self.resp_mesg.get_message()
+            print(Fore.GREEN+"Received:\r\n"+str(self.resp_mesg))
+            if self.resp_mesg.get(35,1).decode('utf-8') == "0":
+                print("Hearbeat Response")
+            elif self.resp_mesg.get(35,1).decode('utf-8') == "1":
+                print("Test Request")
+                self.testreqid = self.resp_mesg.get(112,1)
+                self.testRequest(self.testreqid)
+            elif self.resp_mesg.get(35,1).decode('utf-8') == "2":
+                print("send resend request")
+            elif self.resp_mesg.get(35,1).decode('utf-8') == "3":
+                print("Message Rejected, Exiting the program")
+                exit(1)
+            elif self.resp_mesg.get(35,1).decode('utf-8') == "4":
+                print("Sequence Reset..")
+            elif self.resp_mesg.get(35,1).decode('utf-8') == "5":
+                print("Logout")
+            elif self.resp_mesg.get(35,1).decode('utf-8') == "A":
+                print("Logged in successfully")
+            elif self.resp_mesg.get(35,1).decode('utf-8') == "D":
+                print("Order Single ")
+            else:
+                print(self.resp_mesg)
+                print("Proceed based on response value of 35")
+                del self.resp_mesg
+                self.resp_mesg = simplefix.FixParser()
+
         return 
 
     def testRequest(self,resultingheartbeatinresp):
@@ -129,20 +126,19 @@ class socketConnect():
     def processFix(self,custom_fix):
         self._custom_fix = custom_fix
         self._custom_fix = self._custom_fix.split("")
-        if(self._custom_fix != None or self._custom_fix != "\r\n"):
-            self.processed_msg = self.header()
-            self._custom_fix.remove(self._custom_fix[-1])
-            for i in self._custom_fix:
-                self.processed_msg.append_string(i)
-            self._processed_msg = self.processed_msg
-            del self.processed_msg
-        else:
-            self._processed_msg = None
+        self.processed_msg = self.header()
+        self._custom_fix.remove(self._custom_fix[-1])
+        for i in self._custom_fix:
+            self.processed_msg.append_string(i)
+        self._processed_msg = self.processed_msg
+        del self.processed_msg
         return self._processed_msg
 
     def hearbeat(self):
         while True:
+
             if self.counter + timedelta(seconds=5) < datetime.now():
+                print(1)
                 self.header_fix_message = self.header()
                 self.header_fix_message.append_pair(35,0)
                 print(Fore.BLUE+"message sent as hearbeat"+str(self.header_fix_message))
@@ -180,8 +176,9 @@ class socketConnect():
         x.start()
         y = threading.Thread(target = self.hearbeat)
         y.start()
-        z = threading.Thread(target=self.recvMesg)           
-        z.start()               
+        z = threading.Thread(target = self.recvMesg)
+        z.start()
+                      
         return
          
    
